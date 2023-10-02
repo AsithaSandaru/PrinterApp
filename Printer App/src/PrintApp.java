15fdbada30faed7405e638ef358f34790b2fa7fd
AsithaSandaru <143335815+AsithaSandaru@users.noreply.github.com>
2023-10-02 12:45:39 +0300

    Initial Commit

---
+++ Printer App/src/PrintApp.java
@@ -0,0 +1,24 @@
+
+public class PrintApp {
+
+	public static void main(String[] args) {
+		String Creator ="Asitha";
+		int number1 = 11;
+		int number2 = 2;
+		int multiplication = number1*number2;
+		int addition = number1+number2;
+		int subs = number1-number2;
+		double divi = (double) number1;
+		double divi2 = (double) number2;
+		double division= (double) number1/(double) number2;
+		System.out.println("This app is made by:"+Creator);
+		//System.out.println(number1);
+		System.out.println("Number 1 is "+number1);
+		System.out.println("Number 1 is "+number2);
+		System.out.println(number1+"* "+number2+"="+multiplication);
+		System.out.println(number1+"+ "+number2+"="+addition);
+		System.out.println(divi+"/ "+divi2+"="+division);
+		System.out.println(number1+"- "+number2+"="+subs);
+	}
+
+}
